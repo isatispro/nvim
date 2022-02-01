5c518816a9ff9f842850172a9cf770a81f7e72ff
isatispro <isatispro@qq.com>
2022-02-01 21:22:57 +0800

    some new

---
+++ install.sh
@@ -0,0 +1,6 @@
+yes | sudo apt install nodejs exuberant-ctags cscope npm ranger ruby python3 pip multimon llvm clangd clangd-format
+yes | sudo npm install yarn
+yes | sudo gen install neovim 
+yes | sudo pip install pynvim neovim
+yes | sudo pip install pynvim ueberzug
+
