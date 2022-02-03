41cc2b4b414256b419e918df5f6515d3710b1585
isatispro <isatispro@qq.com>
2022-02-03 22:27:32 +0800

    some new

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-yes | sudo apt install nodejs exuberant-ctags cscope npm ranger ruby python3 pip multimon llvm clangd clangd-format
+yes | sudo apt install nodejs exuberant-ctags cscope npm ranger ruby python3 pip multimon llvm clangd clang-format ripgrep
 yes | sudo npm install yarn
 yes | sudo gen install neovim 
 yes | sudo pip install pynvim neovim
