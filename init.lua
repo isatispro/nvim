e17c8da54846f5f52f5e90bfc93af883b7e667c3
isatispro <isatispro@qq.com>
2022-02-06 02:11:00 +0800

    lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+require('option')
+require('plugins')
+require('keymapping')
+
+require('config/bufferline')
+require('config/nvim-tree')
+-- require('config/nvim-treesitter')
+require('config/telescope')
+require('config/nvim-autopairs')
+require('config/comment')
+
---
+++ lua/plugins.lua
@@ -0,0 +1,84 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use {
+      'wbthomason/packer.nvim'
+  }
+    
+  -- nvim-tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  -- treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  
+  -- bufferline
+  use {
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    -- setup = function() require('config.bufferline') end
+  }
+
+  -- Comment
+  use 'numToStr/Comment.nvim'
+
+  -- nvim-autopairs
+  use 'windwp/nvim-autopairs'
+    --------------------------- colorscheme ------------------------------------
+  -- gruvbox
+  use {
+    "ellisonleao/gruvbox.nvim",
+    requires = {"rktjmp/lush.nvim"},
+    config = 'vim.cmd([[colorscheme gruvbox]])'
+  }
+  -- zephyr
+  use 'glepnir/zephyr-nvim'
+  -- nord
+  use 'shaunsingh/nord.nvim'
+  -- onedark
+  use 'ful1e5/onedark.nvim'
+
+  -------------------------- lsp -------------------------------------------
+
+  -- lspconfig
+  use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
+  -- nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
+  use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
+  use 'hrsh7th/cmp-path'     -- { name = 'path' }
+  use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
+  use 'hrsh7th/nvim-cmp'
+  -- vsnip
+  use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
+  use 'hrsh7th/vim-vsnip'
+  use 'rafamadriz/friendly-snippets'
+  -- lspkind
+  use 'onsails/lspkind-nvim'
+  -- use 'github/copilot.vim'
+
+  use 'simrat39/rust-tools.nvim'
+  
+  -- telescope
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/plenary.nvim'}}
+  }
+  ------------------------------------
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
